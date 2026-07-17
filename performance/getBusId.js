5235021ba203198c599ad0c354dbf0d23680d3cf
Akhil Ramanan(UST <307391@ust.com>
2026-07-18 00:38:51 +0530

    fix: url

--- performance/getBusId.js
+++ performance/getBusId.js
@@ -2,31 +2,31 @@ import https from "node:https";
 import fs from "fs";
 
 const url =
-  "https://tripstack.doomple.com/api/buses?from=BOM&to=DEL";
+    "https://tripstack.doomple.com/api/buses?from=BOM&to=DEL";
 
 https.get(url, (res) => {
 
-  let data = "";
+    let data = "";
 
-  res.on("data",
-    chunk => data += chunk
-  );
-
-  res.on("end", () => {
-
-    const json = JSON.parse(data);
-
-    const busId =
-      json.buses[0].id;
-
-    fs.writeFileSync(
-      "busid.txt",
-      busId
+    res.on("data",
+        chunk => data += chunk
     );
 
-    console.log(
-      `Bus Id: ${busId}`
-    );
-  });
+    res.on("end", () => {
+
+        const json = JSON.parse(data);
+
+        const busId =
+            json.buses[0].id;
+
+        fs.writeFileSync(
+            "busid.txt",
+            busId
+        );
+
+        console.log(
+            `Bus Id: ${busId}`
+        );
+    });
 
 });
