0a566ccb1285c1b5aae1a2b69b5b2992240d252c
Akhil Ramanan(UST <307391@ust.com>
2026-07-18 11:00:05 +0530

    change url to tripstack

--- performance/seatmap-browser.js
+++ performance/seatmap-browser.js
@@ -20,7 +20,7 @@ export default async function () {
   const page = await browser.newPage();
 
   try {
-    await page.goto('https://test.k6.io/');
+    await page.goto('https://tripstack.doomple.com/buses/BUS-BOMDEL-04/seatmap');
     await page.screenshot({ path: 'screenshots/screenshot.png' });
   } finally {
     await page.close();
