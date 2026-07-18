06762648280171cd7b1eab2322618f28b0c0405b
Akhil Ramanan(UST <307391@ust.com>
2026-07-18 10:56:14 +0530

    k6 test

--- performance/seatmap-browser.js
+++ performance/seatmap-browser.js
@@ -1,12 +1,9 @@
 import { browser } from 'k6/browser';
-import { check } from 'k6';
 
 export const options = {
   scenarios: {
-    seatmap_render: {
+    ui: {
       executor: 'shared-iterations',
-      vus: 1,
-      iterations: 5,
       options: {
         browser: {
           type: 'chromium',
@@ -14,44 +11,18 @@ export const options = {
       },
     },
   },
+  thresholds: {
+    checks: ['rate==1.0'],
+  },
 };
 
 export default async function () {
   const page = await browser.newPage();
 
   try {
-
-    await page.goto(
-      'https://tripstack.doomple.com/buses/BUS-BOMDEL-04/seatmap'
-    );
-
-    await page.locator('[data-seat]').first().waitFor();
-
-    const metrics = await page.evaluate(() => {
-      const nav =
-        performance.getEntriesByType('navigation')[0];
-
-      return {
-        loadTime: nav.loadEventEnd,
-        domContentLoaded: nav.domContentLoadedEventEnd
-      };
-    });
-
-    console.log(
-      `Load Time = ${metrics.loadTime} ms`
-    );
-
-    check(metrics.loadTime, {
-      'seatmap < 3000ms': (v) => v < 3000,
-    });
-
+    await page.goto('https://test.k6.io/');
+    await page.screenshot({ path: 'screenshots/screenshot.png' });
   } finally {
     await page.close();
   }
-}
-
-export function handleSummary(data) {
-  return {
-    'seatmap-summary.json': JSON.stringify(data, null, 2),
-  };
 }
